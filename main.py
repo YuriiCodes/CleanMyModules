22c4d9b13a982ac7c7362b29ec322b00c377aa8b
YuriiCodes <yuriipidlisnyi2021@gmail.com>
2023-07-13 15:17:55 +0300

    add the sum-up of a deletioin pop up

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
+from PyQt6.QtGui import QIcon
 from PyQt6.QtWidgets import QApplication, QPushButton, QVBoxLayout, QWidget, QListWidgetItem, QListWidget, QFileDialog, QLineEdit, QHBoxLayout, QLabel, QSizePolicy, QProgressDialog, QMessageBox
 from PyQt6.QtCore import Qt
 import os
 import shutil
+
+def get_size(start_path = '.'):
+    total_size = 0
+    for dirpath, dirnames, filenames in os.walk(start_path):
+        for f in filenames:
+            fp = os.path.join(dirpath, f)
+            # skip if it is symbolic link
+            if not os.path.islink(fp):
+                total_size += os.path.getsize(fp)
+    return total_size
 
 class App(QWidget):
     def __init__(self):
@@ -12,7 +23,7 @@ class App(QWidget):
 
     def initUI(self):
         self.setWindowTitle(self.title)
-
+        self.setWindowIcon(QIcon('/logo.png'))
         # Create widgets
         self.layout = QVBoxLayout()
         self.select_directory_button = QPushButton('Select Directory')
@@ -77,6 +88,7 @@ class App(QWidget):
         progress.setValue(100)  # End progress dialog
 
     def delete(self):
+        total_size = 0  # Initialize total size
         progress = QProgressDialog("Deleting...", "Cancel", 0, self.modules_list.count(), self)  # New progress dialog
         progress.setWindowModality(Qt.WindowModality.WindowModal)
         progress.setValue(0)
@@ -88,12 +100,14 @@ class App(QWidget):
             item = self.modules_list.item(i)
             if item.checkState() == Qt.CheckState.Checked:
                 if os.access(item.text(), os.W_OK):  # Check if we have write access to the directory
+                    total_size += get_size(item.text())  # Add size of directory to total size
                     shutil.rmtree(item.text(), ignore_errors=True)
                     progress.setValue(self.modules_list.count() - i)  # Update progress dialog
                     self.modules_list.takeItem(i)
                 else:  # If we don't have write access, show an error message
                     QMessageBox.critical(self, "Error", f"Don't have permission to delete {item.text()}")
         progress.reset()  # Reset the progress dialog
+        QMessageBox.information(self, "Success", f"You successfully cleaned {total_size / (1024 * 1024):.2f}MB.")  # Show cleaned size
 
     def select_all(self):  # New slot
         for i in range(self.modules_list.count()):
